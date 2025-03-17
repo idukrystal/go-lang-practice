9a36f259c897ccab55a342e70f3ecc6521f50526
idukrystal <70081912+idukrystal@users.noreply.github.com>
2025-03-17 13:31:30 +0000

    start contxt and http practice

---
+++ http-context/server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Handler Started")
+	defer log.Print("Handler Ended")
+	time.Sleep(5 * time.Second)
+	fmt.Println(w, "Hello")
+}
