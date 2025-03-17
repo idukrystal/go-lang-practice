46452c662c601fc02409b6dae98f4f0506fa2def
idukrystal <70081912+idukrystal@users.noreply.github.com>
2025-03-17 11:49:24 +0000

    practice context

---
+++ context/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	go func() {
+		var s string
+		fmt.Scan(&s)
+		cancel()
+	}()
+	sleepAndTalk(ctx, 5*time.Second, "Hello")
+
+}
+
+func sleepAndTalk(ctx context.Context, d time.Duration, msg string) {
+L:
+	for {
+		select {
+		case <-after(d):
+			fmt.Println(msg)
+			break L
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			break L
+		default:
+			fmt.Println("Hold on...")
+		}
+	}
+
+}
+
+func after(d time.Duration) chan bool {
+	c := make(chan bool)
+	go func() {
+		time.Sleep(d)
+		fmt.Println("sending out now")
+		c <- true
+		fmt.Println("sent")
+	}()
+	return c
+}
