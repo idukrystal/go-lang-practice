6e14768b2057b923b5dfcdbba5190e92f886e980
Christopher Idunoba <idukrystal@gmail.com>
2025-08-03 22:04:31 +0100

    ch 8 learning go

---
+++ learning-go/generics/test.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type Converter[T any, U any] interface {
+	Convert(t T) U
+}
+
+
+func ConvertSlice[T any, U any](c Converter[T, U], slice []T) []U {
+	newSlice := make([]U, len(slice))
+	for i, val := range slice {
+		newSlice[i] = c.Convert(val)
+	}
+	return newSlice
+}
+
+
+type StringToIntConvFunc func(string) int
+func (sticf StringToIntConvFunc) Convert(s string) int {
+	return sticf(s)
+}
+func getLength(s string) int  {return len(s)}
+
+type multiplier struct {
+	mul int
+}
+func (m multiplier) Convert(i int) int { 
+	return m.mul * i
+}
+
+type a struct{}
+
+func main() {
+	names := []string{ "Mike", "Joy", "Paulina", "James"}
+
+	converter := StringToIntConvFunc(getLength)
+
+	nameLengths := ConvertSlice(converter, names)
+	var gg *a = nil
+	print(gg)
+	doubler := multiplier{2}
+
+	fmt.Println(ConvertSlice(doubler, nameLengths))
+}
